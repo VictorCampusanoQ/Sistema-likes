68ffe085f43116b546f4fdb9abaf628ea9bd6875
VictorCampusanoQ <victor.campusano.q@gmail.com>
2023-09-26 16:11:13 -0300

    sistema de likes

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+function addLike(elemento){
+    //Aumentar likes
+    let like = document.querySelector("#like").innerText;
+    like ++;
+    document.querySelector("#like").innerText = like;
+    
+}
+function addLike1(elemento){
+    //Aumentar likes
+    let like = document.querySelector("#like1").innerText;
+    like ++;
+    document.querySelector("#like1").innerText = like;
+    
+}
+function addLike2(elemento){
+    //Aumentar likes
+    let like = document.querySelector("#like2").innerText;
+    like ++;
+    document.querySelector("#like2").innerText = like;
+    
+}
+function addLike3(elemento){
+    //Aumentar likes
+    let like = document.querySelector("#like3").innerText;
+    like ++;
+    document.querySelector("#like3").innerText = like;
+    
+}
